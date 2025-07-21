4569a7517c8c9636365fde1c4464471d1f62bfd8
Mayank <mayanksharma4174@gmail.com>
2025-07-21 16:42:05 +0530

    Fully removed Clerk, back to Firebase auth clean

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,5 @@
-import { ClerkProvider } from '@clerk/nextjs';
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
-  );
+  return <Component {...pageProps} />;
 }
-
