ac10408b7805f87eb68eaade5ae7d1d5bb1a52b3
Mayank <mayanksharma4174@gmail.com>
2025-07-21 16:28:37 +0530

    Add AdSense verification snippet

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,1 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document';
 
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* AdSense verification snippet */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5849061465214269"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
